38196e5cc1b47a28b18da8f4a1c6888bc37597db
Fahad Touseef <touseef@wisc.edu>
2025-04-28 11:51:42 -0500

    debugging

--- train.py
+++ train.py
@@ -99,12 +99,10 @@ def main():
 
     # Define pipeline layers
     layers = [
-        # Add a lambda function to unpack input_ids from the tuple
-        lambda x : print(type(x), x.shape, x.dtype, x),  # Debugging line
-        # lambda inputs: inputs[0],  # Unpack input_ids from the tuple
         model.backbone.embedding, 
         *list(model.backbone.layers),
         model.backbone.norm_f,
+        lambda x: x[:, 0, :],  # Extract the [CLS] token representation
         model.classifier
     ]
 
